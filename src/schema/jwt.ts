29db0d4ff596f109da0265a9145a8a064dbac3b1
Roshni-acc <roshnisinghnov24@gmail.com>
2025-08-31 19:37:09 +0530

    Added schmea for jwt.io

---
+++ src/schema/jwt.ts
@@ -0,0 +1,10 @@
+export interface User{
+    id : string ,
+    email : string ,
+    password : string ,
+    created_at: Date,
+    created_by :string | null ,
+    updated_at: string | null ,
+    updated_by:Date,
+
+}
