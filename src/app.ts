7e264f7dc3ef07c34cc0a048bec8056ffc7006d2
Roshni-acc <roshnisinghnov24@gmail.com>
2025-09-07 20:37:01 +0530

    Fixed env file

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ mongoose.connect(process.env.MONGO_URI!, {
 .catch(err => console.error(err));
 
 const app: Application = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
