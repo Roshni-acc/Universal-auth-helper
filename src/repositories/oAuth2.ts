0c4a16033e64d8d1ea03962364f24d62852f8bd7
Roshni-acc <roshnisinghnov24@gmail.com>
2025-09-15 23:20:15 +0530

    Added repo filee

--- src/repositories/oAuth2.ts
+++ src/repositories/oAuth2.ts
@@ -5,8 +5,6 @@ export class UserRepository {
   async findByProvider(provider: string,providerId: string): Promise<IUser | null> {
     return UserModel.findOne({ provider, providerId });
   }
-
-
   async findByEmail(email:string): Promise<IUser | null>{
     return UserModel.findOne({ email });
   }
