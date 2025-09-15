5f6ef4aae314de04a85778d59825577c3de333c4
Roshni-acc <roshnisinghnov24@gmail.com>
2025-09-15 22:31:35 +0530

    Added schema for auth2 file

---
+++ src/schema/oAuth2.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email?: string;
+  password?: string;   // for JWT users (optional)
+  provider?: string;   // e.g. "google", "github"
+  providerId?: string; // unique ID from OAuth provider
+  name?: string;
+}
+
+const UserSchema = new Schema<IUser>({
+  email: { type: String, required: false },
+  password: { type: String, required: false },
+  provider: { type: String, required: false },
+  providerId: { type: String, required: false },
+  name: { type: String, required: false },
+});
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
